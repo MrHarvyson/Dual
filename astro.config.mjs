2c92566808f4a59f116e9d01cb2319e08ca07152
José Carlos Moreno Martín <josecmoreno3d@gmail.com>
2025-05-18 23:31:24 +0200

    cambio inciio

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,30 +6,30 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-      starlight({
-          title: 'My Docs',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-          sidebar: [
-              {
-                  label: 'Inicio',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Introducción', slug: 'inicio/introduccion_inicio', badge: { text: 'Importante', variant: 'danger' } },
-                  ],
-              },
-              {
-                  label: 'Maven',
-                  items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Introducción', slug: 'maven/introduccion_maven' },
-                      { label: 'Estructura', slug: 'maven/estructura_maven' },
-                      { label: 'Creación', slug: 'maven/creacion_maven' },
-                  ],
-              },
-          ],
-      }),
+	integrations: [
+		starlight({
+			title: 'VEDRUNA',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/MrHarvyson/Dual' }],
+			sidebar: [
+				{
+					label: 'Inicio',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Introducción', slug: 'inicio/introduccion_inicio', badge: { text: 'Importante', variant: 'danger' } },
+					],
+				},
+				{
+					label: 'Maven',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Introducción', slug: 'maven/introduccion_maven' },
+						{ label: 'Estructura', slug: 'maven/estructura_maven' },
+						{ label: 'Creación', slug: 'maven/creacion_maven' },
+					],
+				},
+			],
+		}),
 	],
 
-  adapter: vercel(),
+	adapter: vercel(),
 });
