0c2b2450d1692e8576eea30e0fbd93d4424effe0
José Carlos Moreno Martín <josecmoreno3d@gmail.com>
2025-05-18 23:19:24 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,34 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'My Docs',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+          sidebar: [
+              {
+                  label: 'Inicio',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Introducción', slug: 'inicio/introduccion_inicio', badge: { text: 'Importante', variant: 'danger' } },
+                  ],
+              },
+              {
+                  label: 'Maven',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Introducción', slug: 'maven/introduccion_maven' },
+                      { label: 'Estructura', slug: 'maven/estructura_maven' },
+                      { label: 'Creación', slug: 'maven/creacion_maven' },
+                  ],
+              },
+          ],
+      }),
 	],
+
+  adapter: vercel(),
 });
